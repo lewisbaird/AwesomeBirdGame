9fd20951a334095cdc099df388b3aee6613fa59d
lewis <19032@wc.school.nz>
2022-03-31 12:31:31 +1300

    Fixed background + added more
    Added an additional background and fixed the speed of the current rotating background.

--- AwesomeBirdGameAwesome/Assets/Scripts/BackgroundRotating.cs
+++ AwesomeBirdGameAwesome/Assets/Scripts/BackgroundRotating.cs
@@ -4,15 +4,17 @@ using UnityEngine;
 
 public class BackgroundRotating : MonoBehaviour
 {
+    public float speed = 0.0005f;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.Rotate(0, -1, 0);
+        transform.Rotate(0, -speed, 0);
     }
 }
