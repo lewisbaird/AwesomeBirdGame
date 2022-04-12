1aff6fc1491b694e393f892e3d9cb2508fc11d15
lewis <19032@wc.school.nz>
2022-04-13 10:24:15 +1200

    fixes

--- AwesomeBirdGameAwesome/Assets/Scripts/SpawnManager.cs
+++ AwesomeBirdGameAwesome/Assets/Scripts/SpawnManager.cs
@@ -5,23 +5,22 @@ using UnityEngine;
 public class SpawnManager : MonoBehaviour
 {
     public GameObject[] ObstaclePrefabs;
-    public int ObstacleIndex;
     private float RockXPosition = -35;
-    private float RockYPosition = 10;
     private float RockZPosition = 11;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.D))
+        if (Input.GetKeyDown(KeyCode.K))
         {
-            Instantiate(ObstaclePrefabs[ObstacleIndex], new Vector3(RockXPosition, RockYPosition, RockZPosition), ObstaclePrefabs[ObstacleIndex].transform.rotation);
+            Instantiate(ObstaclePrefabs[0], new Vector3(RockXPosition, Random.Range(13, 5), RockZPosition), ObstaclePrefabs[0].transform.rotation);
         }
     }
 }
+
